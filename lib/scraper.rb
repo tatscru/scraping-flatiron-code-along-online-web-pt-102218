5c666f3257e13b7c67018742afd5f4daf8fab557
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-16 17:00:25 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,48 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    #should only do one thing per method 
+  end 
+
+  def get_courses
+    self.get_page.css(".post")
+    #operated on the HTML page.. so you should refer to .get_page
+    # puts doc aka [.get_page].css("")
+  end 
+  
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new 
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text 
+    course.description = post.css("p").text 
+    
+    #instantiating Course objects + giving each course the correct title, schedule, + description
+  end 
+end 
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
-      end
+        #you will need to use pry so you find the right CSS selectors 
     end
   end
+end 
   
 end
+
+Scraper.new.print_courses
 
 
 

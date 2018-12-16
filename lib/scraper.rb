3d9f1253cae6faec92718d27174e516faaf85e46
tatiana.mojica <tatiana.mojica@bustle.com>
2018-12-16 16:16:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-# require 'pry'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -9,7 +9,7 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    # binding.pry 
+    binding.pry 
   end 
 end 
 
